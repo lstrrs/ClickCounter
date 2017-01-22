13819d0cd0da58cda74d6e4dd24d4bf234de5e36
Torres <luitor@amazon.com>
2017-01-21 17:21:58 -0800

    Initial Commit

---
+++ ClickCounter/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ClickCounter
+//
+//  Created by Torres, Luis on 1/21/17.
+//  Copyright © 2017 Torres, Luis. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
